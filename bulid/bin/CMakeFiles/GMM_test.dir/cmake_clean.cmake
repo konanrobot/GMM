55aacd714d356c355d8ed2a47bed8f11e4001e21
konanrobot <konanrobot@163.com>
2015-06-28 14:53:41 +0800

    Yel修改

---
+++ bulid/bin/CMakeFiles/GMM_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/GMM_test.dir/main.cpp.o"
+  "../../bin/GMM_test.pdb"
+  "../../bin/GMM_test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/GMM_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ bulid/lib/CMakeFiles/GMM.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/GMM.dir/KMeans.cpp.o"
+  "CMakeFiles/GMM.dir/GMM.cpp.o"
+  "libGMM.pdb"
+  "libGMM.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/GMM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
